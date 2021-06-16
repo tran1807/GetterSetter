b90f7fa26f0de0e50b974d0a0109396aab6a6ad3
github <nhutranle59@gmail.com>
2021-06-16 09:52:36 +0700

    first commit

--- src/advance/dev/MainApp.java
+++ src/advance/dev/MainApp.java
@@ -3,43 +3,53 @@ package advance.dev;
 import java.util.Scanner;
 
 public class MainApp {
-	static final int NUMBER_STUDENT = 10;
 	static void input(Student[] students) {
 		Scanner sc = new Scanner(System.in);
-		for (int i = 0; i < NUMBER_STUDENT; i++) {
+		for(int i = 1; i < students.length; i++) {
 			System.out.println("Sinh vien thu: " + i);
-			System.out.println("Nhap ten : ");
-			String name = sc.next();
-			System.out.println("Nhap tuoi : ");
-			int age = sc.nextInt();
-			System.out.println("Nhap dia chi : ");
-			String dc = sc.next();
-			System.out.println("Nhap so dien thoai : ");
-			int sdt = sc.nextInt();
-			System.out.println("Nhap diem trung binh : ");
-			float dtb = sc.nextFloat();
-			Student Std = new Student(name, age, dc, sdt, dtb);
+			students[i] = new Student(); 
+	
+			System.out.println("name:");
+			students[i].getName();
+			students[i].name = sc.next();
+	
+			System.out.println("age:");
+			students[i].getAge();
+			students[i].age = sc.nextInt();
+	
+			System.out.println("dia chi:");
+			students[i].getDc();
+			students[i].dc = sc.next();
+	
+			System.out.println("So dien thoai:");
+			students[i].getSdt();
+			students[i].sdt = sc.nextInt();
+	
+			System.out.println("Diem trung binh:");
+			students[i].getDtb();
+			students[i].dtb = sc.nextDouble();
 		}
 	}
-	static void print(Student[] students) {
-		for (int i = 0; i < students.length; i++) {
-			System.out.println(students[i].toString());
+	static void print(Student[] std) {
+		for(int i = 1; i < std.length; i++) {
+			System.out.println(std[i].getName() + " " + std[i].getAge() + " " + std[i].getDc() + " " + std[i].getSdt() + " " + std[i].getDtb());	
 		}
 	}
-	static void diemTrungBinh(Student[] students) {
-	      Student preMax = students[0];
+	static void sapXep(Student[] students ) {
+		Student preMax = students[0];
 		for (int i = 0; i < students.length; i++) {
-			if(preMax.getDtb() < students[i].toString()) {
-				preMax=students[i];
+			if(preMax.getDtb() > students[i].getDtb()) {
+				preMax = students[i];
 			}
 		}
 	}
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       Student[] students = new Student[NUMBER_STUDENT];
-       input(students);
-       print(students);
+		Student[] students = new Student[10];
+		input(students);
+		print(students);
+		sapXep(students);
 	}
-
-}
+	
+	}
--- src/advance/dev/Student.java
+++ src/advance/dev/Student.java
@@ -1,65 +1,40 @@
 package advance.dev;
 
 public class Student {
-	 private String name;
-	 private int age;
-	 private String dc;
-	 private int sdt ;
-	 private double dtb;
-	 
-	public Student(String name, int age, String dc, int sdt, double dtb) {
-		super();
-		this.name = name;
-		this.age = age;
-		this.dc = dc;
-		this.sdt = sdt;
-		this.dtb = dtb;
-	}
-
+	public String name;
+	public int age;
+	public String dc;	
+	public int sdt;
+	public double dtb;
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public int getAge() {
 		return age;
 	}
-
 	public void setAge(int age) {
 		this.age = age;
 	}
-
 	public String getDc() {
 		return dc;
 	}
-
 	public void setDc(String dc) {
 		this.dc = dc;
 	}
-
 	public int getSdt() {
 		return sdt;
 	}
-
 	public void setSdt(int sdt) {
 		this.sdt = sdt;
 	}
-
 	public double getDtb() {
 		return dtb;
 	}
-
 	public void setDtb(double dtb) {
 		this.dtb = dtb;
 	}
-	@Override
-	public String toString() {
-		String toString = null;
-		// TODO Auto-generated method stub
-		return toString.format("diem trung binh la",dtb);
-	}
+	
 }
-
